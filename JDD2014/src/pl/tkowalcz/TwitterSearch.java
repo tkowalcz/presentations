410da2d1850c8148f4dbf9f85dc0b1a57839994e
Tomasz Kowalczewski <tomasz.kowalczewski@codewise.com>
2014-09-24 19:58:12 +0200

    Working on examples fo JDD2014

--- JDD2014/src/pl/tkowalcz/TwitterSearch.java
+++ JDD2014/src/pl/tkowalcz/TwitterSearch.java
@@ -1,4 +1,33 @@
 package pl.tkowalcz;
 
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+import retrofit.http.Query;
+import se.akerfeldt.signpost.retrofit.RetrofitHttpOAuthConsumer;
+import se.akerfeldt.signpost.retrofit.SigningOkClient;
+
+import java.util.List;
+
 public class TwitterSearch {
+
+	public static void main(String[] args) {
+		RetrofitHttpOAuthConsumer oAuthConsumer = new RetrofitHttpOAuthConsumer("", "");
+		oAuthConsumer.setTokenWithSecret("", "");
+
+		RestAdapter restAdapter = new RestAdapter.Builder()
+				.setEndpoint("https://api.twitter.com/1.1")
+				.setLogLevel(RestAdapter.LogLevel.FULL)
+				.setClient(new SigningOkClient(oAuthConsumer))
+				.build();
+
+		TwitterUserSearch service = restAdapter.create(TwitterUserSearch.class);
+		List<String> reactive = service.searchUsers("Reactive");
+		System.out.println(reactive);
+	}
+}
+
+interface TwitterUserSearch {
+
+	@GET("/users/search.json")
+	List<String> searchUsers(@Query("q") String user);
 }
