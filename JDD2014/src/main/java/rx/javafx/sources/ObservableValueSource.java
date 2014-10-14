91c0abb58c7b7b2b71a23d7943018cb1262950e7
Tomasz Kowalczewski <tomasz.kowalczewski@codewise.com>
2014-10-14 21:20:36 +0200

    Removed dependency on rxjavafx-0.0.1-dev.5+0.x-SNAPSHOT.jar, imported required files from https://github.com/ReactiveX/RxJavaFX

---
+++ JDD2014/src/main/java/rx/javafx/sources/ObservableValueSource.java
@@ -0,0 +1,45 @@
+package rx.javafx.sources;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.subscriptions.JavaFxSubscriptions;
+
+/**
+ * Imported from https://github.com/ReactiveX/RxJavaFX
+ */
+public class ObservableValueSource {
+
+    /**
+     * @see rx.observables.JavaFxObservable#fromObservableValue
+     */
+    public static <T> Observable<T> fromObservableValue(final ObservableValue<T> fxObservable) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
+            @Override
+            public void call(final Subscriber<? super T> subscriber) {
+                subscriber.onNext(fxObservable.getValue());
+
+                final ChangeListener<T> listener = new ChangeListener<T>() {
+                    @Override
+                    public void changed(final ObservableValue<? extends T> observableValue, final T prev, final T current) {
+                        subscriber.onNext(current);
+                    }
+                };
+
+                fxObservable.addListener(listener);
+
+                subscriber.add(JavaFxSubscriptions.unsubscribeInEventDispatchThread(new Action0() {
+                    @Override
+                    public void call() {
+                        fxObservable.removeListener(listener);
+                    }
+                }));
+
+            }
+        });
+    }
+
+
+}
